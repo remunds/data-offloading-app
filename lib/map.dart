29fe14afd0c3afd9d6d1ec6ac63afc58f5916f51
lennart <lennartfrederikoestreich@gmail.com>
2020-12-14 14:45:08 +0100

    added map. Widget in lower corner is WIP

---
+++ lib/map.dart
@@ -0,0 +1,83 @@
+import 'package:data_offloading_app/boxinfopage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong/latlong.dart" as latLng;
+import 'package:user_location/user_location.dart';
+
+//MyMap class
+class MyMap extends StatefulWidget {
+  @override
+  _MyMapState createState() => _MyMapState();
+}
+
+//MyMap state to keep track of the _boxes list
+class _MyMapState extends State<MyMap> {
+  @override
+  MapController mapController = MapController();
+  List<Marker> _boxes = [];
+  UserLocationOptions userLocationOptions;
+
+  Widget build(BuildContext context) {
+    userLocationOptions = UserLocationOptions(
+      context: context,
+      mapController: mapController,
+      markers: _boxes,
+    );
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new latLng.LatLng(50.8022, 8.7668),
+        zoom: 13.0,
+        plugins: [
+          //to track user
+          UserLocationPlugin(),
+        ],
+      ),
+      layers: [
+        new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']),
+        //add list of markers to map
+        new MarkerLayerOptions(markers: _boxes),
+        userLocationOptions,
+      ],
+      mapController: mapController,
+    );
+  }
+
+  //Wrapper funktion to build a marker for a sensor box.
+  //pass latitude and longitude as parameter
+  Marker _buildBoxMarker(double lat, double long) {
+    return new Marker(
+      width: 20.0,
+      height: 20.0,
+      point: new latLng.LatLng(lat, long),
+      builder: (ctx) => Container(
+        //GestureDetector to detect if marker is clicked
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          child: new Icon(
+            Icons.location_on,
+            size: 20.0,
+            color: Colors.brown,
+          ),
+          onTap: () {
+            //push the boxinfo page to the navigator
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => BoxInfoPage()));
+          },
+        ),
+      ),
+    );
+  }
+
+  //used to initialize the _boxes List. In the real application the entires from the DB would be read at this point.
+  @protected
+  @mustCallSuper
+  void initState() {
+    _boxes.add(_buildBoxMarker(50.8050, 8.7669));
+    _boxes.add(_buildBoxMarker(50.8160, 8.7669));
+    _boxes.add(_buildBoxMarker(50.8080, 8.7769));
+    _boxes.add(_buildBoxMarker(50.8280, 8.7769));
+    _boxes.add(_buildBoxMarker(50.8080, 8.7869));
+  }
+}
