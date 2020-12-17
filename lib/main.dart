d864e888e8e6f1b77431b27bf93bbc472e595ddd
lennart <lennartfrederikoestreich@gmail.com>
2020-12-17 15:42:51 +0100

    rebase done

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,9 @@ import 'package:permission_handler/permission_handler.dart';
 
 import 'package:provider/provider.dart';
 
-import 'Screens/map.dart';
-
 import 'provider/box_connection_state.dart';
 import 'widgets/home.dart';
-import 'Screens/settings.dart';
+import 'Screens/map.dart';
 
 void main() => runApp(MultiProvider(
       providers: [
